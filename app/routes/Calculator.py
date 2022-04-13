edce7110d755f259d0ccd3e774b3e1f73e355fb9
raymondtam01 <s_raymond.tam@ousd.org>
2022-04-13 12:39:25 -0700

    wensday update

--- app/routes/Calculator.py
+++ app/routes/Calculator.py
@@ -1,7 +1,9 @@
+from app import app
+from flask import render_template
+
 
 print( "1 Addition" )
 print( "2 subtraction " )
 print( "3 multiplication" )
 print( "4 division" )
 
-choice = import ("enter your choice")
