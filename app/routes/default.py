a87cd971f8a320aa17733787992cb83cfa8086d2
lucyach <achesonlucy@gmail.com>
2022-04-22 11:41:20 -0700

    route to todolist

--- app/routes/default.py
+++ app/routes/default.py
@@ -6,6 +6,10 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
-@app.route('/aboutus.html')
+@app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
+
+@app.route('/todolist')
+def aboutus():
+    return render_template('todolist.html')
