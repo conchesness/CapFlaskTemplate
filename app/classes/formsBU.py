0339ed93866c5872c43763e1a7a37bad8fe57de8
conchesness <37635142+conchesness@users.noreply.github.com>
2022-05-09 16:14:53 -0700

    Delete uneeded login forms

--- app/classes/formsBU.py
+++ app/classes/formsBU.py
@@ -11,47 +11,6 @@ from wtforms.validators import URL, NumberRange, Email, Optional, InputRequired,
 from wtforms import PasswordField, StringField, SubmitField, TextAreaField, HiddenField, IntegerField, SelectField, FileField, BooleanField
 from app.classes.data import User
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me?')
-    submit = SubmitField()
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])  
-    fname = StringField('First Name', validators=[DataRequired()])
-    lname = StringField('Last Name', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
-
-    def validate_username(self, username):
-        try:
-            User.objects.get(username=username.data)
-        except mongoengine.errors.DoesNotExist:
-            flash(f"{username.data} is available.")
-        else:
-            raise ValidationError('This username is taken.')
-
-    def validate_email(self, email):
-        try:
-            User.objects.get(email=email.data)
-        except mongoengine.errors.DoesNotExist:
-            flash(f'{email.data} is a unique email address.')
-        else:
-            raise ValidationError('This email address is already in use. If you have forgotten your credentials you can try to recover your account.')
-
-class ResetPasswordRequestForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Request Password Reset')
-
-class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Request Password Reset')
-
 class ProfileForm(FlaskForm):
     fname = StringField('First Name', validators=[DataRequired()])
     lname = StringField('Last Name', validators=[DataRequired()]) 
