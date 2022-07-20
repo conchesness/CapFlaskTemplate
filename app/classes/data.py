89ad3ce88a76d93abbc8153ae53938905c253b36
Steve-Leke <43915535+Steve-Leke@users.noreply.github.com>
2022-07-20 11:55:46 -0500

    Fixed Requirements

--- app/classes/data.py
+++ app/classes/data.py
@@ -68,7 +68,7 @@ class Courses(Document):
     course_number = StringField(required=True,unique=True)
     course_title = StringField()
     course_name = StringField()
-    course_year = IntField()
+    course_year = IntField(required=False, sparse=True)
     course_ag_requirement = StringField()
     course_difficulty = StringField()
     course_department = StringField()
